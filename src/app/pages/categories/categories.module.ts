d011cef9f247af391e35a15faf7bd482f5423459
Renan Steiger <renansteiger@live.com>
2023-08-04 16:13:55 -0300

    feat: improving category list component template

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
-
-
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [
     CategoryListComponent,
@@ -13,7 +13,9 @@ import { CategoryFormComponent } from './category-form/category-form.component';
   ],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    BreadcrumbModule,
+    ButtonModule
   ]
 })
 export class CategoriesModule { }
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  home: MenuItem | undefined;
+
+  ngOnInit(): void {
+    this.items = [{ label: 'Categories' }];
+    this.home = { icon: 'pi text-primary pi-home', routerLink: '/', styleClass: "color-blue" };
+  }
+
+  deleteCategory(categoriaId: any){
+    alert('excluir')
+  }
 }
